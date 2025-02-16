3c04e0ae2829b7a82965f32d3e82d2ff39e861f3
Aguren <sentientfish@gmail.com>
2025-02-15 16:52:22 -0800

    credits menu

--- menu.gd
+++ menu.gd
@@ -22,6 +22,7 @@ func _on_options_button_pressed() -> void:
 
 func _on_credits_button_pressed() -> void:
 	print("Credits Button Pressed!")
+	get_tree().change_scene_to_file("res://credits.tscn")
 
 
 func _on_quit_button_pressed() -> void:
