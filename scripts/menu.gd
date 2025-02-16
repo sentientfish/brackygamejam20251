34c4a6bf6a6678cea15deca7f56ae5511f1f678a
Aguren <sentientfish@gmail.com>
2025-02-16 12:35:14 -0800

    directories

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -17,12 +17,12 @@ func _on_start_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
 	print("Options Button Pressed!")
-	get_tree().change_scene_to_file("res://options.tscn")
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 
 func _on_credits_button_pressed() -> void:
 	print("Credits Button Pressed!")
-	get_tree().change_scene_to_file("res://credits.tscn")
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 
 func _on_quit_button_pressed() -> void:
