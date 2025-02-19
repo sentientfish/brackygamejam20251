281f922ec618feac2c3d4a03664161b7fe3c82e4
Aguren <sentientfish@gmail.com>
2025-02-18 22:50:06 -0800

    put all scenes and some ui

--- scripts/arena.gd
+++ scripts/arena.gd
@@ -1,14 +1,31 @@
 extends Node2D
 
+var win_timer: Timer = null
+var enemy: Node2D = null
+var victory_label: Label = null
+
+var victory_string = "Victory!!!"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	win_timer = get_node("WinTimer")
+	victory_label = get_node("VictoryLabel")
+	victory_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	victory_label.hide()
+	enemy = get_node("Enemy")
+	enemy.connect("enemy_died", _on_enemy_died)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	victory_label.text = victory_string + "\nReturning to Rest Area in\n" + \
+		str(int(win_timer.time_left))
 
-func _on_arena_back_button_pressed() -> void:
-	print("Arena Back Button Pressed!")
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+func _on_win_timer_timeout() -> void:
+	# We win, go to rest area!
+	win_timer.stop()
+	get_tree().change_scene_to_file("res://scenes/rest_area.tscn")
+
+func _on_enemy_died() -> void:
+	# We win, start win timer before moving to new area
+	victory_label.show()
+	win_timer.start()
